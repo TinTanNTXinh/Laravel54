ef6da5b78ca380f83e418ac4523d6276ee2a4399
Xinh Nguyen <ntxinh@tintansoft.com>
2017-05-09 16:07:07 +0700

    update env

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -40,7 +40,7 @@ class DatabaseSeeder extends Seeder
         /*
          * ===========================================
          * */
-        
+
         /*
         ### DEVELOP
         # Nhóm người dùng
