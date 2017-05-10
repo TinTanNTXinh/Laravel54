dfad6c0262c185936d23f8d5c45a9f7e645aed6d
Xinh Nguyen <ntxinh@tintansoft.com>
2017-05-10 14:06:55 +0700

    add code app component (this bug of authenticate for Angular)

--- public/dev/src/app/app.component.ts
+++ public/dev/src/app/app.component.ts
@@ -1,34 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationStart} from '@angular/router';
+import {Subscription} from 'rxjs';
+
+import {AuthenticationService} from './services/authentication.service';
+import {LoggingService} from './services/logging.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
+    private authenticate: boolean = false;
+    private _authSubscription: Subscription;
 
+    constructor(private router: Router, private authenticationService: AuthenticationService, private loggingService: LoggingService) {
+        this._authSubscription = this.authenticationService.authenticate$.subscribe(
+            status => {
+                this.loggingService.consoleLog("%c AppComponent", "color: blue");
+                this.loggingService.consoleLog(status);
+                this.loggingService.consoleLog("%c ------------", "color: blue");
+                this.authenticate = Boolean(status);
+                if (!status) {
+                    router.navigate(['/login']);
+                }
+            }
+        );
 
-  constructor(private router: Router) {
-    
-  }
-
-  ngOnInit() {
-  }
-
-  public displayLayout(): boolean {
-    if (this.router.url == '/login'){
-      return false;
+        router.events.subscribe(
+            event => {
+                if (event instanceof NavigationStart) {
+                    if (event.url == '/login') {
+                        if (this.authenticate) {
+                            this.router.navigate(['/dashboards']);
+                        }
+                    } else {
+                        if (!this.authenticate) {
+                            this.router.navigate(['/login']);
+                        }
+                    }
+                }
+                // NavigationEnd
+                // NavigationCancel
+                // NavigationError
+                // RoutesRecognized
+            }
+        )
     }
-    return true;
-  }
 
-    public setBodyClass(): string {
-        if (this.router.url == '/login'){
-            return 'login-container';
-        }
-        return 'xinh-xinh';
+    ngOnInit() {
+    }
+
+    public displayLayout(): boolean {
+        return !(this.router.url == '/login');
     }
 
 }
