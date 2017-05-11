e41f33db51f5df155115f0e77e2de248c83a82eb
Xinh Nguyen <ntxinh@tintansoft.com>
2017-05-11 11:02:06 +0700

    update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,81 +11,80 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $env = env('APP_PROD', false);
+        if ($env) {
+            ### DEFAULT SEED
+            # Nhóm người dùng
+            $this->call(GroupRolesTableSeeder::class);
+            $this->call(RolesTableSeeder::class);
+            $this->call(PositionsTableSeeder::class);
+            $this->call(AdminsTableSeeder::class);
+            $this->call(AdminRolesTableSeeder::class);
 
-        ### DEFAULT SEED
-        # Nhóm người dùng
-        $this->call(GroupRolesTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PositionsTableSeeder::class);
-        $this->call(AdminsTableSeeder::class);
-        $this->call(AdminRolesTableSeeder::class);
+            # Nhóm thiết bị
+            $this->call(CollectionsTableSeeder::class);
 
-        # Nhóm thiết bị
-        $this->call(CollectionsTableSeeder::class);
+            # Nhóm sản phẩm
+            $this->call(UnitsTableSeeder::class);
 
-        # Nhóm sản phẩm
-        $this->call(UnitsTableSeeder::class);
+            # Nhóm thiết bị - sản phẩm
 
-        # Nhóm thiết bị - sản phẩm
+            # Nhóm thiết bị - người dùng
 
-        # Nhóm thiết bị - người dùng
+            # Nhóm khu vực
 
-        # Nhóm khu vực
+            # Nhóm tập tin
+            $this->call(AdminFilesTableSeeder::class);
 
-        # Nhóm tập tin
-        $this->call(AdminFilesTableSeeder::class);
+            # Nhóm khách hàng
+        } else {
+            /*
+             * ===========================================
+             * */
 
-        # Nhóm khách hàng
+            ### DEVELOP
+            # Nhóm người dùng
+            $this->call(GroupRolesTableSeeder::class);
+            $this->call(RolesTableSeeder::class);
+            $this->call(PositionsTableSeeder::class);
+            $this->call(AdminsTableSeeder::class);
+            $this->call(UsersTableSeeder::class);
+            $this->call(AdminRolesTableSeeder::class);
+            $this->call(UserRolesTableSeeder::class);
 
-        /*
-         * ===========================================
-         * */
+            # Nhóm thiết bị
+            $this->call(CollectionsTableSeeder::class);
+            $this->call(IOCentersTableSeeder::class);
+            $this->call(DevicesTableSeeder::class);
 
-        /*
-        ### DEVELOP
-        # Nhóm người dùng
-        $this->call(GroupRolesTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PositionsTableSeeder::class);
-        $this->call(AdminsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(AdminRolesTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
+            # Nhóm sản phẩm
+            $this->call(ProductTypesTableSeeder::class);
+            $this->call(ProductsTableSeeder::class);
+            $this->call(ProductPricesTableSeeder::class);
+            $this->call(ProducersTableSeeder::class);
+            $this->call(UnitsTableSeeder::class);
 
-        # Nhóm thiết bị
-        $this->call(CollectionsTableSeeder::class);
-        $this->call(IOCentersTableSeeder::class);
-        $this->call(DevicesTableSeeder::class);
+            # Nhóm thiết bị - sản phẩm
+            $this->call(ButtonProductsTableSeeder::class);
+            $this->call(HistoryInputOutputsTableSeeder::class);
 
-        # Nhóm sản phẩm
-        $this->call(ProductTypesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(ProductPricesTableSeeder::class);
-        $this->call(ProducersTableSeeder::class);
-        $this->call(UnitsTableSeeder::class);
+            # Nhóm thiết bị - người dùng
+            $this->call(UserCardsTableSeeder::class);
+            $this->call(UserCardMoneysTableSeeder::class);
 
-        # Nhóm thiết bị - sản phẩm
-        $this->call(ButtonProductsTableSeeder::class);
-        $this->call(HistoryInputOutputsTableSeeder::class);
+            # Nhóm khu vực
+            $this->call(CitiesTableSeeder::class);
+            $this->call(DistrictsTableSeeder::class);
+            $this->call(WardsTableSeeder::class);
+            $this->call(NationsTableSeeder::class);
 
-        # Nhóm thiết bị - người dùng
-        $this->call(UserCardsTableSeeder::class);
-        $this->call(UserCardMoneysTableSeeder::class);
+            # Nhóm tập tin
+            $this->call(AdminFilesTableSeeder::class);
+            $this->call(FilesTableSeeder::class);
 
-        # Nhóm khu vực
-        $this->call(CitiesTableSeeder::class);
-        $this->call(DistrictsTableSeeder::class);
-        $this->call(WardsTableSeeder::class);
-        $this->call(NationsTableSeeder::class);
-
-        # Nhóm tập tin
-        $this->call(AdminFilesTableSeeder::class);
-        $this->call(FilesTableSeeder::class);
-
-        # Nhóm khách hàng
-        $this->call(DistributorsTableSeeder::class);
-        $this->call(SuppliersTableSeeder::class);
-        */
-
+            # Nhóm khách hàng
+            $this->call(DistributorsTableSeeder::class);
+            $this->call(SuppliersTableSeeder::class);
+        }
     }
 }
